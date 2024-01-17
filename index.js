7f341b9f9a68ac06b071e9d5e091b89ffd5237e6
yanbin <yanbinwork@163.com>
2024-01-17 12:11:58 +0800

    优化目录结构

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 #!/usr/bin/env node
-import fs from 'fs'
-import { program } from 'commander'
+import homeOptions from './lib/core/home/index.js'
 
-program.name('nvc').usage('<command> [options]')
-
-// 版本信息
-const version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version
-program.version(version)
-
-program.showHelpAfterError()
-
-program.parse(process.argv)
+homeOptions()
